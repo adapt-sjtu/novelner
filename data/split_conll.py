55b64f0732bd69d6f03459ac0385e0d7db896ce8
Frank Xu <frankxu2004@gmail.com>
2017-06-08 15:36:37 +0800

    update

---
+++ data/split_conll.py
@@ -0,0 +1,17 @@
+import os
+from sklearn.model_selection import train_test_split
+
+conll = open('wnut17train.conll', encoding='utf-8').read().split('\n\t\n')
+
+train, devtest = train_test_split(conll, test_size=0.3)
+
+dev, test = train_test_split(devtest, test_size=0.5)
+
+
+with open('train.conll', 'w', encoding='utf-8') as train_f, open('dev.conll', 'w', encoding='utf-8') as dev_f, open('test.conll', 'w', encoding='utf-8') as test_f:
+	for line in train:
+		train_f.write(line + '\n\t\n')
+	for line in dev:
+		dev_f.write(line + '\n\t\n')
+	for line in test:
+		test_f.write(line + '\n\t\n')
