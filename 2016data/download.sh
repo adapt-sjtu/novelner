920d9f92d3a8ffc0026c1ae73bbe0e16e9d55e92
yuchenlin <yuchenlin@sjtu.edu.cn>
2017-06-16 18:02:19 +0800

    add 2016 wnut data

---
+++ 2016data/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/aritter/twitter_nlp/master/data/annotated/wnut16/data/train
+wget https://raw.githubusercontent.com/aritter/twitter_nlp/master/data/annotated/wnut16/data/dev
+wget https://raw.githubusercontent.com/aritter/twitter_nlp/master/data/annotated/wnut16/data/test
+wget https://raw.githubusercontent.com/aritter/twitter_nlp/master/data/annotated/wnut16/data/train_notypes
+wget https://raw.githubusercontent.com/aritter/twitter_nlp/master/data/annotated/wnut16/data/test_notypes
+wget https://raw.githubusercontent.com/aritter/twitter_nlp/master/data/annotated/wnut16/data/dev_notypes
