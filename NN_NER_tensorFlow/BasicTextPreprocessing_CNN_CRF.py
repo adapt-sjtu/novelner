e691143ec17dca3c3ab8c570a0332bafc2cbb89e
yuchenlin <yuchenlin@sjtu.edu.cn>
2017-06-17 14:47:13 +0800

    new model

--- NN_NER_tensorFlow/BasicTextPreprocessing_CNN_CRF.py
+++ NN_NER_tensorFlow/BasicTextPreprocessing_CNN_CRF.py
@@ -22,7 +22,7 @@ from tensorflow.python import debug as tf_debug
 
 tf.__version__
 
-#usage : python BasicTextPreprocessing_CNN_CRF.py 
+#usage : python BasicTextPreprocessing_CNN_CRF.py
 #here 'word' is the name of the alphabet class instance
 print("Loading data...")
 word_alphabet = Alphabet('word')
@@ -38,10 +38,10 @@ embedding_path = "/home/zhiyi/data/glove.twitter.27B.100d.gz" # "glove.6B.100d.g
 oov = 'embedding'
 fine_tune = True
 # Model Hyperparameters
-#tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)") #not used 
-tf.flags.DEFINE_string("train_path", "Smaple_data/eng.train.iobes.act", "Train Path") # eng.train.iobes.act
-tf.flags.DEFINE_string("test_path", "Smaple_data/eng.testa.iobes.act", "Test Path")  # eng.testa.iobes.act
-tf.flags.DEFINE_string("dev_path", "Smaple_data/eng.testb.iobes.act", "dev Path") # eng.testb.iobes.act
+#tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)") #not used
+tf.flags.DEFINE_string("train_path", "../2016data/train", "Train Path") # eng.train.iobes.act
+tf.flags.DEFINE_string("test_path", "../2016data/test", "Test Path")  # eng.testa.iobes.act
+tf.flags.DEFINE_string("dev_path", "../2016data/dev", "dev Path") # eng.testb.iobes.act
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("grad_clip", 5, "value for gradient clipping to avoid exploding/vanishing gradient(default: 5.0) in LSTM")
 tf.flags.DEFINE_float("max_global_clip", 5.0, "value for gradient clipping to avoid exploding/vanishing gradient overall(default: 1.0)")
@@ -89,14 +89,14 @@ word_sentences_train, _, word_index_sentences_train, label_index_sentences_train
 if oov == "random" and not fine_tune:
     logger.info("Close word alphabet.")
     word_alphabet.close()
-    
+
 
 # read dev data
 logger.info("Reading data from dev set...")
 word_sentences_dev, _, word_index_sentences_dev, label_index_sentences_dev = dp.read_conll_sequence_labeling(
     dev_path, word_alphabet, label_alphabet, word_column, label_column)
 
-# close alphabets : by close we mean we cannot add any more words to the word vocabulary. 
+# close alphabets : by close we mean we cannot add any more words to the word vocabulary.
 #To DO :change to close this after train set alone
 word_alphabet.close()
 label_alphabet.close()
@@ -105,7 +105,7 @@ label_alphabet.close()
 # we are doing a -1 because we did not use the zer index. I believe this is to account for unknown word
 logger.info("word alphabet size: %d" % (word_alphabet.size() - 1))
 logger.info("label alphabet size: %d" % (label_alphabet.size() - 1))
-# get maximum length : this is mainly for padding. 
+# get maximum length : this is mainly for padding.
 max_length_train = utils.get_max_length(word_sentences_train)
 max_length_dev = utils.get_max_length(word_sentences_dev)
 #max_length_test = utils.get_max_length(word_sentences_test)
@@ -125,13 +125,13 @@ label_index_sentences_dev_pad,_= utils.padSequence(label_index_sentences_dev,max
 
 logger.info("Creating character set FROM training set ...")
 char_alphabet = Alphabet('character')
-char_index_train,max_char_per_word_train= dp.generate_character_data(word_sentences_train,  
+char_index_train,max_char_per_word_train= dp.generate_character_data(word_sentences_train,
                                     char_alphabet=char_alphabet,setType="Train")
 # close character alphabet. WE close it because the embed table is goign to be random
 char_alphabet.close()
 
 logger.info("Creating character set FROM dev set ...")
-char_index_dev,max_char_per_word_dev= dp.generate_character_data(word_sentences_dev, 
+char_index_dev,max_char_per_word_dev= dp.generate_character_data(word_sentences_dev,
                                     char_alphabet=char_alphabet, setType="Dev")
 
 
@@ -171,19 +171,19 @@ Flags_Dict['model_path']=out_dir
 # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
 checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
 Flags_Dict['checkpoint_dir']=checkpoint_dir
-dill.dump(Flags_Dict,open(os.path.join(out_dir, "config.pkl"),'wb')) 
-dill.dump(char_alphabet,open(os.path.join(out_dir, "char_alphabet.pkl"),'wb')) 
-dill.dump(word_alphabet,open(os.path.join(out_dir, "word_alphabet.pkl"),'wb')) 
-dill.dump(label_alphabet,open(os.path.join(out_dir, "label_alphabet.pkl"),'wb')) 
+dill.dump(Flags_Dict,open(os.path.join(out_dir, "config.pkl"),'wb'))
+dill.dump(char_alphabet,open(os.path.join(out_dir, "char_alphabet.pkl"),'wb'))
+dill.dump(word_alphabet,open(os.path.join(out_dir, "word_alphabet.pkl"),'wb'))
+dill.dump(label_alphabet,open(os.path.join(out_dir, "label_alphabet.pkl"),'wb'))
 tf.reset_default_graph()
 
 session_conf = tf.ConfigProto(
   allow_soft_placement=FLAGS.allow_soft_placement,
   log_device_placement=FLAGS.log_device_placement)
 with tf.Session(config=session_conf) as sess:
-    best_accuracy = 0 
+    best_accuracy = 0
     best_overall_accuracy = 0
-    best_accuracy_test = 0 
+    best_accuracy_test = 0
     best_overall_accuracy_test = 0
     best_step = 0
     BiLSTM = network.textBiLSTM(sequence_length=max_length, num_classes=num_classes, word_vocab_size=word_vocab_size,
@@ -200,7 +200,7 @@ with tf.Session(config=session_conf) as sess:
         optimizer = tf.train.GradientDescentOptimizer(learning_rate) #also try GradientDescentOptimizer , AdamOptimizer
     elif(FLAGS.Optimizer==1):
         optimizer = tf.train.AdamOptimizer(learning_rate)
-    
+
     #This is the first part of minimize()
     grads_and_vars = optimizer.compute_gradients(BiLSTM.loss)
     #clipped_grads_and_vars = [(tf.clip_by_norm(grad, FLAGS.max_global_clip), var) for grad, var in grads_and_vars]
@@ -227,12 +227,12 @@ with tf.Session(config=session_conf) as sess:
             grad_summaries.append(grad_hist_summary)
             grad_summaries.append(sparsity_summary)
     grad_summaries_merged = tf.summary.merge(grad_summaries)
-    
 
-    
+
+
     # Summaries for loss and accuracy
     loss_summary = tf.summary.scalar("loss", BiLSTM.loss)
-    #acc_summary = tf.summary.scalar("accuracy", BiLSTM.accuracy)  
+    #acc_summary = tf.summary.scalar("accuracy", BiLSTM.accuracy)
 
     # Train Summaries
     train_summary_op = tf.summary.merge([loss_summary, grad_summaries_merged])
@@ -250,16 +250,16 @@ with tf.Session(config=session_conf) as sess:
     if not os.path.exists(checkpoint_dir):
         os.makedirs(checkpoint_dir)
     saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
-    
+
     # variables need to be initialized before we can use them
     sess.run(tf.global_variables_initializer())
 
     #debug block
     #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
     #sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
-        
 
-    
+
+
     def dev_step (session,BiLSTM,PadZeroBegin,max_length,x_batch,y_batch,act_seq_lengths,
         dropout_keep_prob,embedd_table,step,char_batch,char_embedd_table,writer= None):
 
@@ -278,7 +278,7 @@ with tf.Session(config=session_conf) as sess:
         A single training step
         """
         feed_dict=af.create_feed_Dict(BiLSTM,PadZeroBegin,max_length,x_batch,y_batch,act_seq_lengths,dropout_keep_prob,embedd_table,char_batch,char_embedd_table)
-        
+
         _, step, summaries, loss,logits,transition_params = session.run(
             [train_op, global_step, train_summary_op, BiLSTM.loss,BiLSTM.logits,BiLSTM.transition_params],
             feed_dict)
@@ -290,7 +290,7 @@ with tf.Session(config=session_conf) as sess:
     # Generate batches
     batches = utils.batch_iter(
         list(zip(word_index_sentences_train_pad, label_index_sentences_train_pad ,train_seq_length,char_index_train_pad)), FLAGS.batch_size, FLAGS.num_epochs)
-    
+
     # Training loop. For each batch...
     for batch in batches:
         x_batch, y_batch,act_seq_lengths,char_batch = zip(*batch)
@@ -304,7 +304,7 @@ with tf.Session(config=session_conf) as sess:
                                             embedd_table,current_step,char_index_dev_pad,char_embedd_table, writer=dev_summary_writer)
             print("")
             if (accuracy_low_classes > best_accuracy):
-                
+
                 path = saver.save(sess, checkpoint_prefix, global_step=current_step)
                 best_accuracy = accuracy_low_classes
                 best_step = current_step
--- tagger/run.sh
+++ tagger/run.sh
@@ -1,1 +1,1 @@
-./train.py --train ../data/split/train.conll --dev ../data/split/dev.conll --test ../data/split/test.conll
+./train.py --train ../data/split/train.conll.iob --dev ../data/split/dev.conll.iob --test ../data/split/test.conll.iob
