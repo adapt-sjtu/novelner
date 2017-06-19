290c6874f23e901e3647cea3cf974d1f2eb7fabb
yuchenlin <yuchenlin@sjtu.edu.cn>
2017-06-19 11:40:56 +0800

    move models

--- 2016data/2016to2017.sh
+++ 2016data/2016to2017.sh
@@ -3,6 +3,6 @@ python 2016to2017.py dev > dev.2016to2017.conll.iob
 python 2016to2017.py test > test.2016to2017.conll.iob
 cat train.2016to2017.conll.iob ../data/split/train.conll.iob > train.cat.iob
 cat dev.2016to2017.conll.iob ../data/split/dev.conll.iob > dev.cat.iob
-cat test.2016to2017.conll.iob ../data/split/test.conll.iob > test.cat.iob
-cat train.cat.iob test.cat.iob > train.catt.iob
-cat train.cat.iob dev.cat.iob test.cat.iob > train.cattt.iob
+#cat test.2016to2017.conll.iob ../data/split/test.conll.iob > test.cat.iob
+cat train.cat.iob test.2016to2017.conll.iob > train.catt.iob
+cat train.catt.iob dev.2016to2017.conll.iob > train.cattt.iob
