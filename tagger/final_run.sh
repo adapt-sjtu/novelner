7553a3488af31b5865400284ca0abf57f739a249
Yuchen <yuchenlin>
2017-06-24 20:58:31 +0800

    add new feature

--- tagger/final_run.sh
+++ tagger/final_run.sh
@@ -1,4 +1,4 @@
 #OMP_NUM_THREADS=30 python ./train.py --train ../data/final/train.2017.final --dev ../final_data/dev.final --test ../final_data/test.final --pre_emb /home/yuchen/useful_data/glove.twitter.27B.100d.txt --zeros 1 --cap_dim 5 --char_dim 25 --char_lstm_dim 25
 #OMP_NUM_THREADS=30 python ./train.py --train ../data/final/train.2017.final --dev ../final_data/dev.final --test ../final_data/test.final --pre_emb /home/yuchen/useful_data/glove.twitter.27B.100d.txt --zeros 1 --cap_dim 5 --char_dim 25 --char_lstm_dim 25
-OMP_NUM_THREADS=30 python ./train.py --train ../data/final/train.2017.final --dev ../final_data/dev.final --test ../final_data/test.final --pre_emb /home/yuchen/useful_data/glove.twitter.27B.100d.txt --zeros 1 --cap_dim 5 --char_dim 25 --char_lstm_dim 25 --lr_method adam
+OMP_NUM_THREADS=30 python ./train.py --train ../final_data/train.final --dev ../final_data/dev.final --test ../final_data/test.final --pre_emb /home/yuchen/useful_data/glove.twitter.27B.100d.txt --zeros 1 --char_dim 25 --char_lstm_dim 25 --lr_method adam
 # test data will not be used 
